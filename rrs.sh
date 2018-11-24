c973e9fd2e7361cccded3f2ebafecb75d695141e
antinym <ijbshield@gmail.com>
2018-11-24 16:00:22 -0500

    alt version of the quiet test

--- rrs.sh
+++ rrs.sh
@@ -15,7 +15,7 @@ DEFINE_integer 'seconds' '300' 'number of seconds to record' 's'
 DEFINE_integer 'msecs' '30000' 'number of milliseconds to record' 'm'
 DEFINE_integer 'interval' 500 'frequency in milliseconds to record RSSI values' 'i'
 DEFINE_string 'timestamp' 'zero' 'logged timestamp format: (zero | now | none )' 't'
-DEFINE_boolean 'quiet' false 'suppress message output to stdout' 'q'
+DEFINE_boolean 'quiet' true 'suppress message output to stdout' 'q'
 DEFINE_boolean 'overwrite' true 'overwrite old log files' 'o'
 DEFINE_boolean 'headers' true 'prepend logfile with column headers' 'H'
 DEFINE_boolean 'transmit' false 'transmit packets to the access point' 'x'
@@ -26,23 +26,25 @@ DEFINE_string 'file' 'rrs_log.csv' 'file to save the RSSI log results' 'f'
 FLAGS "$@" || exit $?
 eval set -- "${FLAGS_ARGV}"
 
-#LOG_FILE=FLAGS_file
+# sets the delay between readings to seconds (with fractions)
 RSSI_READ_INTERVAL=$(echo "scale=4; $FLAGS_interval/1000" | bc -l)
-#RSSI_READ_COUNT=FLAGS_count
-#LOGGED_START_TIME=FLAGS_timestamp 
+
+# sets the proper timestamp to prepend to each reading
 [[ $FLAGS_timestamp == "zero" ]] && START_TIME=$(date +%s%3N) || START_TIME=0
-[[ $FLAGS_quiet ]] && display_path="/dev/null" || display_path="/dev/fd/3"
-# echo $display_path; exit 0;
 
+# toggles whether to print output to the screen as well as the log file
+if [ $FLAGS_quiet -eq $FLAGS_TRUE ]; then 
+	display_path="/dev/null"; 
+else 
+	display_path="/dev/fd/3";
+fi
 
-# This parses specific to Raspbian 9 (stretch), the value reported for quality
+# This parser is specific to Raspbian 9 (stretch), the value reported for quality
 #		level is determined by the wireless nic's driver. If you want any other
 #		values reported in /proc/net/wireless just add their respective capture
 #		class ($3 should be quality link and $5 quality noise).
-capture_rssi()
+capture_rssi_by_counter()
 {
-  exec 3>&1 1>>${FLAGS_file}
-  [[ $FLAGS_headers -eq $FLAGS_TRUE ]] && echo "time,RSSI"
 	if [ ${FLAGS_timestamp} != "none" ]; then
 		for i in `seq 1 ${FLAGS_count}`;
 		do
@@ -58,6 +60,28 @@ capture_rssi()
 	fi
 }
 
+
+capture_rssi_on_timer()
+{
+	if [ ${FLAGS_timestamp} != "none" ]; then
+		for i in `seq 1 ${FLAGS_count}`;
+		do
+			awk -F ".[ ]+" -v date="$(($(date +%s%3N)-START_TIME))" 'NR==3 {print date "," $4}'	/proc/net/wireless | tee $display_path
+			sleep $RSSI_READ_INTERVAL
+		done
+	else
+		for i in `seq 1 ${FLAGS_count}`;
+		do
+			awk -F ".[ ]+" 'NR==3 {print $4}' /proc/net/wireless | tee $display_path
+			sleep $RSSI_READ_INTERVAL
+		done
+	fi
+}
+
+
+##
+# Handles creating & preparing new log files if needed. Also tests for write access.
+##
 init_log_file()
 {
 	if [ -w "$FLAGS_file" ]; then
@@ -65,17 +89,25 @@ init_log_file()
 			rm $FLAGS_file
 			touch $FLAGS_file
 		fi
-	else
-		echo "Error! Cannot create the logfile $FLAGS_file."
+	elif [ -e "$FLAGS_file" ]; then
+		echo "Error! Cannot overwrite the logfile $FLAGS_file."
 		exit 1;
+	else
+		touch $FLAGS_file
 	fi
+
+  exec 3>&1 1>>${FLAGS_file}
+  [[ $FLAGS_headers -eq $FLAGS_TRUE ]] && echo "time,RSSI"
 }
 
 ########
 # Main #
 ########
 init_log_file
-capture_rssi
+
+# Counter or Timer?
+capture_rssi_by_counter
+#capture_rssi_on_timer
 
 
 ###### old commands I might need ######
